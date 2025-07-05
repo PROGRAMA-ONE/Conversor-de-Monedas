f566279e901e5feb27c62a48981c66c8e91a83b8
Martha Bermudez <bemm990503@gmail.com>
2025-07-05 17:29:39 -0600

    analizando la respuesta en formato JSON, Filtrando las monedas, convertiendo los valores e intercambiando con el usuario

--- src/ConsultaConversorDeMonedas.java
+++ src/ConsultaConversorDeMonedas.java
@@ -1,25 +1,21 @@
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaConversorDeMonedas {
-    public ConversorDeMonedas buscaConversorDeMonedas(String monedaBase, String monedaDeDestino, float cantidad){
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/e9e79a52ba02745a02fbbda8/pair/"+monedaBase+"/"+monedaDeDestino+"/"+cantidad);
+    public ConversorDeMonedas buscaConversorDeMonedas(String monedaBase, String monedaDeDestino, float cantidad) throws IOException, InterruptedException {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/e9e79a52ba02745a02fbbda8/pair/" + monedaBase + "/" + monedaDeDestino + "/" + cantidad);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
 
-        try {
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            return new Gson().fromJson(response.body(), ConversorDeMonedas.class);
-        } catch (Exception e) {
-            throw new RuntimeException("No encontré esa conversión de monedas.");
-        }
+        return new Gson().fromJson(response.body(), ConversorDeMonedas.class);
     }
 }
